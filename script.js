b43fb302f342ef16bcdea34726e41c7290ee606a
techinalhamza <techinalhamza@gmail.com>
2024-05-01 21:21:16 +0500

    download functionality

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
 let inp = document.querySelector("#inp");
 let img = document.querySelector("#img");
 let msg = document.querySelector(".massage");
+let download = document.querySelector("#download");
 
 async function myFun() {
   let value = inp.value;
   if (value) {
-    msg.innerHTML = `qr code generating... for ${value}`;
+    msg.innerHTML = `QR code generating for ${value}`;
     img.src = `https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${value}`;
     inp.value = "";
 
@@ -15,8 +16,20 @@ async function myFun() {
       };
     });
     await promise;
-    msg.innerHTML = `qr code generated for ${value}`;
+
+    msg.innerHTML = `QR code generated for ${value}`;
+
+    // Create a blob URL for the image
+    let blob = await fetch(img.src).then((res) => res.blob());
+    let url = window.URL.createObjectURL(blob);
+
+    // Set the href and download attributes for the download link
+    download.href = url;
+    download.download = "qrcode.png";
+
+    // Display the download link
+    download.style.display = "block";
   } else {
-    alert("please enter something to generate qr code");
+    alert("Please enter something to generate QR code");
   }
 }
